84c199a0147b9f31ff4360d7b08d2bd29c78dc38
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-09-07 18:19:40 -0500

    init system component on working

---
+++ src/components/initSystem/InitSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PixelDeskLogo from '../../assets/pixel-desk-logo.svg';
+
+import './initSystem.scss';
+
+export const Initsystem = () => {
+  return (
+    <div className='init-system'>
+      <img src={PixelDeskLogo} alt='' />
+    </div>
+  );
+};

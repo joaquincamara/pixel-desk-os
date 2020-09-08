b8c14b2ff3efdb2c7dfc6d040c2a4a81754e4816
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-09-08 09:31:06 -0500

    Updates

---
+++ precache-manifest.f052d75df008d320888c0145708c3670.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3543e47d04027728806505526fa31c",
+    "url": "/pixel-desk-os/index.html"
+  },
+  {
+    "revision": "d86d670675f808aa670e",
+    "url": "/pixel-desk-os/static/css/main.eed7d042.chunk.css"
+  },
+  {
+    "revision": "6bd6daef217c0e3abaa9",
+    "url": "/pixel-desk-os/static/js/2.192fd66a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixel-desk-os/static/js/2.192fd66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86d670675f808aa670e",
+    "url": "/pixel-desk-os/static/js/main.395e600a.chunk.js"
+  },
+  {
+    "revision": "9b23b195b64f0edb3434",
+    "url": "/pixel-desk-os/static/js/runtime-main.96a35c67.js"
+  },
+  {
+    "revision": "b084d6a5cd63a239f40652ba689b7a2c",
+    "url": "/pixel-desk-os/static/media/castlevania.b084d6a5.mp3"
+  },
+  {
+    "revision": "42f57a8c19ff2409dbb534ccc99ca9a0",
+    "url": "/pixel-desk-os/static/media/pixel-desk-logo.42f57a8c.svg"
+  },
+  {
+    "revision": "ad18d75f48af2711c953703a8c760b49",
+    "url": "/pixel-desk-os/static/media/pokemon.ad18d75f.mp3"
+  },
+  {
+    "revision": "35e5073ad1f200cdd2dbc140c734df49",
+    "url": "/pixel-desk-os/static/media/zelda.35e5073a.mp3"
+  }
+]);

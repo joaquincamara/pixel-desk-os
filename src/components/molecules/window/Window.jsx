bc039af954034d8d92cf5df03c16c80ae1056017
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-09-13 21:03:59 -0500

    draggable window done, functions need refactor

--- src/components/molecules/window/Window.jsx
+++ src/components/molecules/window/Window.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '98.css';
 
 import './window.scss';
 
 export const Window = () => {
+  useEffect(() => {
+    dragElement(document.getElementById('mydiv'));
+  });
+
+  function dragElement(elmnt) {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    if (document.getElementById(elmnt.id + 'header')) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+      elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+    }
+
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
   return (
-    <div className='window'>
-      <div className='title-bar'>
+    <div id='mydiv' className='window'>
+      <div id='mydivheader' className='title-bar'>
         <div className='title-bar-text'>System Info</div>
         <div className='title-bar-controls'>
           <button aria-label='Minimize' />
@@ -15,7 +62,9 @@ export const Window = () => {
         </div>
       </div>
 
-      <div className='window-body'></div>
+      <div className='window-body'>
+        <h3>I AM DRAGABBLE</h3>
+      </div>
     </div>
   );
 };
